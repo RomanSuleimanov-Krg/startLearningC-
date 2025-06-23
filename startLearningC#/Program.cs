32acc7ab00ac911b37312128337a990d395a6488
RomanSuleimanov <sulroman2006@mail.ru>
2025-06-24 00:25:08 +0700

    Первый текст в консоль и типы данных

--- startLearningC#/Program.cs
+++ startLearningC#/Program.cs
@@ -10,6 +10,36 @@ namespace startLearningC_
     {
         static void Main(string[] args)
         {
+            //Выводит текст в ковывчках, в консоль
+            Console.WriteLine("Hello World!!!");
+
+            //Основные типы данных int | float | char | string | bool
+
+            //Типы данных:
+            //Целочисленные:
+            //byte b;
+            //sbyte sb;
+            //short s;
+            //ushort us;
+            //int i;
+            //uint ui;
+            //long l;
+            //ulong ul;
+            //Числа с плавающей точкой:
+            //float f;
+            //double d;
+            //Символьные типы данных:
+            //char c;
+            //Строковый тип:
+            //string str;
+            //Логический тип:
+            //bool bl;
+
+            //Инициализация переменной age
+            int age = 27;
+            Console.WriteLine(age);
+
+
         }
     }
 }
