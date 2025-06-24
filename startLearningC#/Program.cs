9dc6061c668a8b388f06eeb68c73caafe8dbdf5d
RomanSuleimanov <sulroman2006@mail.ru>
2025-06-24 23:45:57 +0700

    Закреплял темы и изучил if

--- startLearningC#/Program.cs
+++ startLearningC#/Program.cs
@@ -36,10 +36,60 @@ namespace startLearningC_
             //bool bl;
 
             //Инициализация переменной age
-            int age = 27;
-            Console.WriteLine(age);
+            //int age;
+            //age = 27;
+            //Console.WriteLine(age);
 
+            //Работа с консолью
+            string name;
+            name = Console.ReadLine();
+            Console.WriteLine(name);
 
+            //Закрепление изученного
+            //int health;
+            //int armor;
+            //int damage;
+
+            //Console.Write("Введите кол-во здоровья: ");
+            //health = Convert.ToInt32(Console.ReadLine());
+            //Console.Write("Введите кол-во брони: ");
+            //armor = Convert.ToInt32(Console.ReadLine());
+            //Console.Write("Введите кол-во дамага: ");
+            //damage = Convert.ToInt32(Console.ReadLine());
+
+            //health = health - (damage - armor);
+
+            //Console.WriteLine(health);
+
+            //_______________________________________________
+
+            //int food;
+            //int money;
+            //int foodUnitPrice;
+
+            //foodUnitPrice = 10;
+
+            //Console.WriteLine($"Еда по {foodUnitPrice} монет");
+            //Console.Write("Сколько у вас голды?");
+            //money = Convert.ToInt32(Console.ReadLine());
+            //Console.Write("Сколько вам нужно еда?");
+            //food = Convert.ToInt32(Console.ReadLine());
+
+            //Console.WriteLine($"У вас теперь {food} еды и осталось {money - foodUnitPrice * food}");
+
+            //Логический оператор If
+            int age;
+            int needAge = 18;
+            age = 15;
+            if (age >= needAge)
+            {
+                Console.WriteLine("Всё норм");
+            }
+
+            else
+            {
+                Console.WriteLine("Ты слишком молод");
+            }
         }
     }
 }
